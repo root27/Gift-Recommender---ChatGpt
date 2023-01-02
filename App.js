d52f46236964d396671e36ededb9d4000bc7172a
root27 <oguzhaneee@gmail.com>
2023-01-02 23:58:38 +0300

    started

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React,{useState} from 'react';
 
 export default function App() {
+
+  const [age, setAge] = useState("");
+  const [gender,setGender] = useState("");
+  const [priceMin,setPriceMin] = useState("");
+  const [priceMax,setPriceMax] = useState("");
+  const [hobbies,setHobbies] = useState("");
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Welcome to the gift generator </Text>
+      <Text style={styles.gender}>Gender</Text>
+      <Text style={styles.male} >Male</Text>
+      <Text style={styles.female} >Female</Text>
+      
+
+
+
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +37,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  age:{},
+  gender:{},
+  priceMin:{  },
+  priceMax:{  },
+
 });
